b17b27db96a32fafba29bc893a600a006e9d0162
maprohu <maprohu@gmail.com>
2015-02-26 20:32:26 +0000

    embedding wars

--- base/core/src/main/java/star/back/StarBackConfiguration.java
+++ base/core/src/main/java/star/back/StarBackConfiguration.java
@@ -2,13 +2,14 @@ package star.back;
 
 import org.cwatch.service.CwatchServiceConfiguration;
 import org.cwatch.split.CwatchSplitConfiguration;
+import org.cwatch.wars.CwatchWarsConfiguration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @PropertySource({"classpath:/star-back.properties"})
-@Import({CwatchServiceConfiguration.class, CwatchSplitConfiguration.class})
+@Import({CwatchServiceConfiguration.class, CwatchSplitConfiguration.class, CwatchWarsConfiguration.class})
 public class StarBackConfiguration {
 	
 }
