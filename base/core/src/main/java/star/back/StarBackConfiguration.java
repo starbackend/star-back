1b17caa13d6f09eab0556df32b4fa98a66452fec
maprohu <maprohu@gmail.com>
2015-02-27 12:35:12 +0000

    adminweb

--- base/core/src/main/java/star/back/StarBackConfiguration.java
+++ base/core/src/main/java/star/back/StarBackConfiguration.java
@@ -1,15 +1,16 @@
 package star.back;
 
+import org.cwatch.activemq.CwatchActivemqConfiguration;
 import org.cwatch.service.CwatchServiceConfiguration;
 import org.cwatch.split.CwatchSplitConfiguration;
-import org.cwatch.wars.CwatchWarsConfiguration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @PropertySource({"classpath:/star-back.properties"})
-@Import({CwatchServiceConfiguration.class, CwatchSplitConfiguration.class, CwatchWarsConfiguration.class})
+@Import({CwatchServiceConfiguration.class, CwatchSplitConfiguration.class, CwatchActivemqConfiguration.class})
 public class StarBackConfiguration {
+	
 	
 }
