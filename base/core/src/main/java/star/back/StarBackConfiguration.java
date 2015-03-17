14e61c59a4327019051d0f3fb9d626698c761082
maprohu <maprohu@gmail.com>
2015-03-17 16:03:45 +0000

    add cdf forward

--- base/core/src/main/java/star/back/StarBackConfiguration.java
+++ base/core/src/main/java/star/back/StarBackConfiguration.java
@@ -2,14 +2,22 @@ package star.back;
 
 import org.cwatch.activemq.CwatchActivemqConfiguration;
 import org.cwatch.service.CwatchServiceConfiguration;
+import org.cwatch.service.cdf.CwatchCdfForwardRouteBuilder;
 import org.cwatch.split.CwatchSplitConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jms.JmsAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @PropertySource({"classpath:/star-back.properties"})
-@Import({CwatchServiceConfiguration.class, CwatchSplitConfiguration.class, CwatchActivemqConfiguration.class})
+@Import({
+	CwatchSplitConfiguration.class, 
+	CwatchServiceConfiguration.class,
+	CwatchCdfForwardRouteBuilder.class, 
+	CwatchActivemqConfiguration.class
+})
 public class StarBackConfiguration {
 	
 	
